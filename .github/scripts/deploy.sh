39bc430faa2d78874bf5691e1f0b6b63534d2090
chrisdca <chrisanderdev@gmail.com>
2025-10-16 22:46:41 -0500

    add deploy ways

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,6 +1,17 @@
+BRANCH_NAME=$1
+
+if [ "$BRANCH_NAME" == "main" ]; then
+  PWD_PATH="/home/azureuser/learning-github-actions"
+if [ "$BRANCH_NAME" == "certi" ]; then
+  PWD_PATH="/home/azureuser/certi"
+else
+  PWD_PATH="/home/azureuser/develop"
+fi
+
+
 ssh -o StrictHostKeyChecking=no azureuser@40.82.162.21 <<HTML
 
-  cd /home/azureuser/learning-github-actions
-  git pull origin main
+  cd $PWD_PATH
+  git pull origin $BRANCH_NAME
 
 HTML
