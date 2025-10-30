b18cb21f567e720660c1a86da445b9a3be43052c
chrisdca <chrisanderdev@gmail.com>
2025-10-30 15:40:45 -0500

    feat: update app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>
-          My first deploy with Github Actions and use my own action v1 !!!
-        </h1>
+        <h2>
+          My first deploy with Github Actions and use my own action v1 !!! 🙉
+        </h2>
+        <p>HELLO!!!!!!!!</p>
+        <small>BYE!!!!!</small>
       </main>
     </div>
   );
